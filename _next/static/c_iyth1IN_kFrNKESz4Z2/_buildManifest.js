856c12107d75ffff40c044862f19bb5500d04a6d
Lighteros <Lightningdev722@gmail.com>
2024-03-31 10:20:01 -0400

    first commit

---
+++ _next/static/c_iyth1IN_kFrNKESz4Z2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/94726e6d-6a6b860420dd6fab.js","static/chunks/662-304ee70b56e7b620.js","static/chunks/pages/index-22dd7ce396df9ef8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
